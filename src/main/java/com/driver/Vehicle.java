7bc177d8f0abcf3baea93c2a4f88d3adf68d9701
kamlesh-cyber <kamleshgupta2804@gmail.com>
2023-06-21 13:45:38 +0530

    assignment done 5

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -40,4 +40,20 @@ public class Vehicle {
     public int getCurrentDirection() {
         return currentDirection;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
 }
